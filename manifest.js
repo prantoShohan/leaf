45c5300e3aca9b30e3f57279e3ef70db8c065408
prantoShohan <me.pranto@gmail.com>
2023-07-13 22:20:36 -0700

    Updates

--- manifest.js
+++ manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png","image/1.png","image/10.png","image/11.png","image/12.png","image/13.png","image/14.png","image/15.png","image/16.png","image/2.png","image/3.png","image/4.png","image/5.png","image/6.png","image/7.png","image/8.png","image/9.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.75faec5e.js","app":"_app/immutable/entry/app.ca69c8fe.js","imports":["_app/immutable/entry/start.75faec5e.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/singletons.48adcd4e.js","_app/immutable/entry/app.ca69c8fe.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.7cf2deec.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.64f8e372.js","app":"_app/immutable/entry/app.360c4291.js","imports":["_app/immutable/entry/start.64f8e372.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/singletons.148e283d.js","_app/immutable/entry/app.360c4291.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.7cf2deec.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- nodes/1.js
+++ nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.6c84f161.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.7cf2deec.js","_app/immutable/chunks/singletons.48adcd4e.js"];
+export const imports = ["_app/immutable/nodes/1.34da7a44.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.7cf2deec.js","_app/immutable/chunks/singletons.148e283d.js"];
 export const stylesheets = [];
 export const fonts = [];
